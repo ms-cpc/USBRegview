e3437d55d95302d86573f725ff076d85c98a26f7
MS <52162055+ms-cpc@users.noreply.github.com>
2022-04-13 11:58:19 -0400

    Add files via upload

--- USBRegview.bat
+++ USBRegview.bat
@@ -1,9 +1,21 @@
 @ECHO OFF
+@setlocal enableextensions
+@cd /d "%~dp0
 TITLE USBRegview
-:: v20220413
+:: v20220413b
 :: Batch file created by Sgt. Mark Southby
 :: Eric Zimmermans RLA used to create Clean SYSTEM Hive
 :: Nirsoft USBDeview used to load clean SYSTEM hive for analysis
+if _%1_==_payload_  goto :payload
+:getadmin
+    echo %~nx0: ELEVATING TO ADMINISTRATOR PROMPT...
+    set vbs=%temp%\getadmin.vbs
+    echo Set UAC = CreateObject^("Shell.Application"^)                >> "%vbs%"
+    echo UAC.ShellExecute "%~s0", "payload %~sdp0 %*", "", "runas", 1 >> "%vbs%"
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+goto :eof
+:payload
 IF EXIST REG\SYSTEM GOTO logcheck
 :notfound
 ECHO Place SYSTEM, SYSTEM.LOG1 and SYSTEM.LOG2 into REG directory.
